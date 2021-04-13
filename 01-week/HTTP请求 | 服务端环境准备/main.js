3de4a33a29748a19fc16f4217d38b5c5d25fb60b
chentianxin <16chentianxin@gmail.com>
2021-04-13 16:26:05 +0800

    HTTP请求 | 服务端环境准,添加端口号以及错误处理

--- 01-week/HTTP请求 | 服务端环境准备/main.js
+++ 01-week/HTTP请求 | 服务端环境准备/main.js
@@ -1,21 +1,20 @@
 const { createServer } = require('http')
+const { argv: [, , port = 8080 ] } = process
 
 const app = createServer((req, res) => {
   let body = []
 
   req
-    .on('data', chunk => {
-      body.push(chunk.toString())
-    })
+    .on('data', chunk => body.push(chunk.toString()))
     .on('end', () => {
       body = Buffer.concat(body).toString()
       console.log(`body: ${ body }`)
       res.writeHead(200, {
         'Content-Type': 'text/html',
       })
-      res.end(`Hello World\n`)
+      res.end(`Hello World`)
     })
-
+    .on('error', err => console.error(err))
 })
-app.listen(8080)
-console.log('Server Started')
+app.listen(port)
+console.log(`Server running at: http://localhost:${ port }/ `)
